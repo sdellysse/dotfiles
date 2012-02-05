beaa14c9caed9a5fd92afdf65e5d45e06ff77896
Shawn Dellysse <sdellysse@gmail.com>
2012-02-05 00:45:24 -0500

    more emacs stuff

--- .emacs.d/init.d/font.el
+++
@@ -1,2 +0,0 @@
-;; Consolas is awesome.
-(set-frame-font "Consolas-12.0")
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,38 @@
-(let ((directories '("key-chord" "evil")))
-  (dolist (directory directories)
-    (add-to-list 'load-path (expand-file-name
-      (concat "~/.emacs.d/packages/" directory "/")
-    ))
-  )
-)
+; Define the directory of this file so that hard paths aren't referenced
+; everywhere.
+(defconst emacs-config-directory (file-name-directory load-file-name))
 
-(let ((init-d (expand-file-name "~/.emacs.d/init.d/")))
-  (byte-recompile-directory init-d 0)
-  (let ((files (directory-files init-d t "\\.elc$")))
-    (dolist (file files)
-      (load file)
-    )
-  )
-)
+; Convenience function to get full path of file in this directory
+; (or subdirectory).
+(defun expand-config-file-name (name)
+  (concat emacs-config-directory "/" name))
+
+; Convenience function to add a directory under `./packages` to the
+; `load-path`
+(defun add-package-directory (directory)
+  (add-to-list 'load-path (expand-config-file-name (concat "packages/" directory))))
+  
+(let ((packages '(
+    color-theme
+    js2-mode
+    magit
+  )))
+  (setq package-archives
+    '(("gnu" . "http://elpa.gnu.org/packages/")
+      ("marmalade" . "http://marmalade-repo.org/packages/")))
+  (package-initialize)
+  (dolist (package-name packages)
+    (when (not (package-installed-p package-name))
+      (package-refresh-contents)
+      (package-install package-name))))
+
+(dolist (path '(
+    "color-theme-vibrant-ink"
+    "evil"
+    "key-chord"
+  ))
+  (add-package-directory path))
+
+; Load each `.el` file in `init.d`. 
+(dolist (file (directory-files (expand-config-file-name "init.d/") t "\\.el$"))
+  (load file)) 
