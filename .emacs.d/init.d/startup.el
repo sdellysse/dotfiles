951c749d41d391611f1df66e2cb46bbf02eeddc4
Shawn Dellysse <sdellysse@gmail.com>
2012-02-05 00:49:03 -0500

    gitignore fixes

---
+++ .emacs.d/init.d/startup.el
@@ -0,0 +1,7 @@
+;; Get rid of startup screen
+(setq
+  inhibit-startup-message t
+  inhibit-startup-echo-area-message t)
+
+(setq backup-directory-alist
+  (list (cons "." (expand-config-file-name "backup-files"))))
