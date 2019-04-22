8a8f9f21bde67013468bb96adf44d5fba286e0bf
Shawn Dellysse <shawn.dellysse@underarmour.com>
2019-04-22 00:41:41 -0400

    fixed umask, added asdf

--- bash_noninteractive.sh
+++ bash_noninteractive.sh
@@ -1,3 +1,9 @@
+# WSL doesn't set a sane umask, fix that here
+if [ "$( umask )" = "0000" ]; then
+  umask 0002
+fi
+
+# Make sure XDG is set up correctly
 [ "$XDG_CONFIG_HOME" != "" ] || export XDG_CONFIG_HOME="${HOME}/config"
 [ "$XDG_DATA_HOME"   != "" ] || export XDG_DATA_HOME="${HOME}/.local/share"
 [ "$XDG_CACHE_HOME"  != "" ] || export XDG_CACHE_HOME="${HOME}/.cache"
@@ -6,18 +12,20 @@ export HISTFILE="${XDG_CACHE_DIR}/bash_history"
 
 
 # auto-install nvm
-if [ ! -e "${XDG_DATA_HOME}/nvm/nvm.sh" ]; then
+if [ ! -e "${HOME}/.nvm/nvm.sh" ]; then
 	echo "Install nvm..."
-	git clone https://github.com/creationix/nvm "${XDG_DATA_HOME}/nvm"
-	git -C "${XDG_DATA_HOME}/nvm" checkout $( git describe --abbrev=0 --tags --match "v[0-9]*" $( git rev-list --tags --max-count=1 ) )
+	git clone https://github.com/creationix/nvm "${HOME}/.nvm"
+	pushd "${HOME}/.nvm"
+	git checkout $( git describe --abbrev=0 --tags --match "v[0-9]*" $( git rev-list --tags --max-count=1 ) )
+	popd
 fi
 
 # Defer initialization of nvm until nvm, node or a node-dependent command is
 # run. Ensure this block is only run once if .bashrc gets sourced multiple times
 # by checking whether __init_nvm is a function.
 # https://www.growingwiththeweb.com/2018/01/slow-nvm-init.html
-if [ -s "${XDG_DATA_HOME}/nvm/nvm.sh" ] && [ ! "$(type -t __init_nvm)" = function ]; then
-  export NVM_DIR="${XDG_DATA_HOME}/nvm"
+if [ -s "${HOME}/.nvm/nvm.sh" ] && [ ! "$(type -t __init_nvm)" = function ]; then
+  export NVM_DIR="${HOME}/.nvm"
 
   declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
 
--- fish/conf.d/001-earlypatches.fish
+++ fish/conf.d/001-earlypatches.fish
@@ -1,3 +1,9 @@
+# WSL doesn't set a sane umask; set this as early as possible
+if [ ( umask ) = "0000" ]
+	umask 0002
+end
+
+# Make sure XDG is set
 [ "$XDG_DATA_HOME"   != "" ] || set --global --export XDG_DATA_HOME "$HOME/.local/share"
 [ "$XDG_CONFIG_HOME" != "" ] || set --global --export XDG_CONFIG_HOME "$HOME/.config"
 [ "$XDG_CACHE_HOME"  != "" ] || set --global --export XDG_CACHE_HOME "$HOME/.cache"
