c029018a88d6cba9a6504b54c9e3d884a9a3710a
Shawn Dellysse <shawn.dellysse@underarmour.com>
2019-04-24 11:45:15 -0400

    added window movement keys, simpler neotree evil config

--- emacs/packages.el
+++ emacs/packages.el
@@ -20,14 +20,19 @@
               :config (progn
                         (setq evil-leader/in-all-states t)
                         (evil-leader/set-leader "<SPC>")
+
+                        (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+                        (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+                        (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+                        (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
                         (evil-mode)))))
 
-(use-package evil-collection
-  :ensure t
-  :after evil
-
-  :config
-  (evil-collection-init))
+;;(use-package evil-collection
+;;  :ensure t
+;;  :after evil
+;;
+;;  :config
+;;  (evil-collection-init))
 
 (use-package evil-escape
   :ensure t
@@ -43,9 +48,15 @@
   :after (evil evil-leader)
 
   :config (progn
+            (setq neo-theme 'nerd)
+
+            (evil-set-initial-state 'neotree-mode 'normal)
             (evil-leader/set-key
               "d" 'neotree-toggle)
 
-            (setq neo-theme 'nerd)
             (add-hook 'neotree-mode-hook (lambda ()
-                                           (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)))))
+                                           (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+                                           (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
+                                           (define-key evil-normal-state-local-map (kbd "o") 'neotree-enter)
+                                           (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
+                                           (define-key evil-normal-state-local-map (kbd "i") 'neotree-enter-horizontal-split)))))
