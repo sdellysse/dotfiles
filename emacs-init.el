3856acd9cf99aa8a05244812276858d84503bf01
Shawn Dellysse <shawn.dellysse@underarmour.com>
2019-05-09 16:16:07 -0400

    cleaned up use-package more

--- emacs-init.el
+++ emacs-init.el
@@ -7,19 +7,6 @@
 (setq custom-file "~/.config/dotfiles/emacs-custom.el")
 (load custom-file)
 
-;;;; Package Repos
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-;;;; Pull in (use-package) if we don't already have it
-(eval-when-compile
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-  (require 'use-package)
-  (setq use-package-always-ensure t))
-
 ;;;; Clean screen startup
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
@@ -37,6 +24,19 @@
 
 ;;;; (y or n) instead of (yes or no)
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;;;; Package Repos
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;;;; Pull in (use-package) if we don't already have it
+(eval-when-compile
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (require 'use-package)
+  (setq use-package-always-ensure t))
 
 ;;;; PACKAGES:::
 ;;;; ============
@@ -58,65 +58,59 @@
   (setq evil-want-keybinding  nil)
 
   :config
-  (progn
-    ;;nest for now, gotta call (evil-mode) after loading evil-leader
-    (use-package evil-leader
-      :init
-      (global-evil-leader-mode)
-
-      :config
-      (progn
-	(setq evil-leader/in-all-states t)
-	(evil-leader/set-leader "<SPC>")
-
-	(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-	(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-	(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-	(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-	(evil-mode)))))
-
-(use-package evil-collection
-  :after (evil magit)
-
-  :config
-  (evil-collection-init 'magit))
+  ;; Don't call (evil-mode) here, it'll be called later by evil-leader
+  (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right))
 
 (use-package evil-escape
-  :after evil
+  :after (evil)
 
   :config 
   (setq-default evil-escape-key-sequence "jk")
   (setq-default evil-escape-unordered-key-sequence t)
   (evil-escape-mode))
 
+(use-package evil-leader
+  :after (evil)
+
+  :init
+  (global-evil-leader-mode)
+
+  :config
+  (setq evil-leader/in-all-states t)
+  (evil-leader/set-leader "<SPC>")
+
+  (evil-mode))
+
 (use-package fish-mode
-  :mode "\\.fish\\'"
+  :mode        "\\.fish\\'"
   :interpreter "fish")
 
-(use-package magit
-  :defer t)
-
 (use-package neotree
-  :after (evil evil-leader)
+  :after    (evil evil-leader)
+  :commands neotree-toggle
+  :hook     (neotree-mode . evil-neotree-hook)
 
-  :config 
-  (setq neo-theme 'nerd)
-
-  (evil-set-initial-state 'neotree-mode 'normal)
+  :init
   (evil-leader/set-key "d" 'neotree-toggle)
 
-  (add-hook 'neotree-mode-hook (lambda ()
-				 (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
-				 (define-key evil-normal-state-local-map (kbd "I")   'neotree-hidden-file-toggle)
-				 (define-key evil-normal-state-local-map (kbd "i")   'neotree-enter-horizontal-split)
-				 (define-key evil-normal-state-local-map (kbd "ma")  'neotree-create-node)
-				 (define-key evil-normal-state-local-map (kbd "mc")  'neotree-copy-node)
-				 (define-key evil-normal-state-local-map (kbd "md")  'neotree-delete-node)
-				 (define-key evil-normal-state-local-map (kbd "mm")  'neotree-rename-node)
-				 (define-key evil-normal-state-local-map (kbd "o")   'neotree-enter)
-				 (define-key evil-normal-state-local-map (kbd "s")   'neotree-enter-vertical-split)
-				 (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-quick-look)
-				 (define-key evil-normal-state-local-map (kbd "u")   'neotree-select-up-node))))
+  :config 
+  (defun evil-neotree-hook () 
+    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+    (define-key evil-normal-state-local-map (kbd "I")   'neotree-hidden-file-toggle)
+    (define-key evil-normal-state-local-map (kbd "i")   'neotree-enter-horizontal-split)
+    (define-key evil-normal-state-local-map (kbd "ma")  'neotree-create-node)
+    (define-key evil-normal-state-local-map (kbd "mc")  'neotree-copy-node)
+    (define-key evil-normal-state-local-map (kbd "md")  'neotree-delete-node)
+    (define-key evil-normal-state-local-map (kbd "mm")  'neotree-rename-node)
+    (define-key evil-normal-state-local-map (kbd "o")   'neotree-enter)
+    (define-key evil-normal-state-local-map (kbd "s")   'neotree-enter-vertical-split)
+    (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-quick-look)
+    (define-key evil-normal-state-local-map (kbd "u")   'neotree-select-up-node))
+  (setq neo-theme 'nerd)
+  (evil-set-initial-state 'neotree-mode 'normal))
 
 (use-package vimrc-mode
   :mode "\\.vim\\(rc\\)?\\'")
